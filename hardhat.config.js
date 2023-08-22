5b4fa8e77d7866bc074510a0358d794f0d49ba5b
julienbrg <julien@strat.cc>
2023-08-22 13:14:22 +0200

    add goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades')
 const dotenv = require("dotenv");
 dotenv.config();
+// require("@nomiclabs/hardhat-etherscan");
 
 const { 
+  GOERLI_TESTNET_ENDPOINT_URL, 
+  GOERLI_TESTNET_PRIVATE_KEY, 
+  GOERLI_ETHERSCAN_API_KEY, 
+
   ARTHERA_TESTNET_PRIVATE_KEY,
   ARTHERA_TESTNET_ENDPOINT_URL,
 } = process.env;
@@ -11,7 +16,7 @@ const {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {  
+module.exports = {
   solidity: {
     version: "0.8.19",
     settings: {
@@ -19,6 +24,18 @@ module.exports = {
         enabled: true,
         runs: 200
       }
+    }
+  },
+  outputSelection: {
+    "*": {
+        "*": [
+          "evm.bytecode",
+          "evm.deployedBytecode",
+          "devdoc",
+          "userdoc",
+          "metadata",
+          "abi"
+      ]
     }
   },
   mocha: {
@@ -29,10 +46,17 @@ module.exports = {
       chainId: 1337,
       allowUnlimitedContractSize: true
     },
+    'goerli': {
+      url: GOERLI_TESTNET_ENDPOINT_URL,
+      accounts: GOERLI_TESTNET_PRIVATE_KEY !== undefined ? [GOERLI_TESTNET_PRIVATE_KEY] : [],
+    },
     'arthera-testnet': {
       url: ARTHERA_TESTNET_ENDPOINT_URL || "",
       chainId: 10243,
       accounts: ARTHERA_TESTNET_PRIVATE_KEY !== undefined ? [ARTHERA_TESTNET_PRIVATE_KEY] : [],
     },
-  }, 
-};
+  },
+  // etherscan: {
+  //   goerli: GOERLI_ETHERSCAN_API_KEY || "",
+  // },
+}
